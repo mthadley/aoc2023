b9958d5bfafb5541e2ce6ef571e1402d969724b9
Michael Hadley <m@mthadley.com>
2023-12-09 14:13:20 -0800

    Day 3

---
+++ bin/day3.lua
@@ -0,0 +1,100 @@
+#!/usr/bin/env nvim -l
+
+local aoc = require "aoc"
+local aoc_string = require "aoc.string"
+local aoc_table = require "aoc.table"
+local Point = require "aoc.point"
+
+local Grid = {}
+
+function Grid.parse(file)
+  local rows = {}
+  for line in io.lines(file) do
+    table.insert(rows, line)
+  end
+
+  return setmetatable({ rows = rows }, { __index = Grid })
+end
+
+function Grid:part_numbers()
+  local numbers = {}
+
+  for y, row in ipairs(self.rows) do
+    local has_symbol = false
+    local number = ""
+
+    for char, x in aoc_string.chars(row) do
+      if char:match("%d") then
+        number = number .. char
+        for _, point in ipairs(Point.new(x, y):principals()) do
+          if point.y > 0 and
+             point.y <= #self.rows and
+             self.rows[point.y]:sub(point.x, point.x):match("[^%.%d]")
+          then
+            has_symbol = true
+            break
+          end
+        end
+      else
+        if number ~= "" and has_symbol then
+          table.insert(numbers, tonumber(number))
+        end
+
+        has_symbol = false
+        number = ""
+      end
+
+      if has_symbol and x >= #self.rows[1] then
+        table.insert(numbers, tonumber(number))
+      end
+    end
+  end
+
+  return numbers
+end
+
+function Grid:ratios()
+  local ratios = {}
+
+  for y, row in ipairs(self.rows) do
+    for char, x in aoc_string.chars(row) do
+      if char == "*" then
+        local numbers = {}
+        for _, point in ipairs(Point.new(x, y):principals()) do
+          local number = ""
+          if point.y > 0 and
+             point.y <= #self.rows and
+             self.rows[point.y]:sub(point.x, point.x):match("%d")
+          then
+            local number_start = point.x
+            for i = point.x, 1, -1 do
+              if not self.rows[point.y]:sub(i, i):match("%d") then break end
+              number_start = i
+            end
+
+            numbers[tonumber(self.rows[point.y]:match("%d+", number_start))] = true
+          end
+        end
+
+        if aoc_table.count(numbers) == 2 then
+          table.insert(
+            ratios,
+            aoc_table.product(numbers, function(_, key) return key end)
+          )
+        end
+      end
+    end
+  end
+
+  return ratios
+end
+
+aoc.play {
+  part1 = function()
+    return aoc_table.sum(Grid.parse("bin/day3.txt"):part_numbers()), 528799
+  end,
+
+  part2 = function()
+    return aoc_table.sum(Grid.parse("bin/day3.txt"):ratios()), 84907174
+  end
+}
--- lua/aoc/table.lua
+++ lua/aoc/table.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 function M.count(tbl, fn)
+  fn = fn or function() return true end
   local count = 0
   for key, val in pairs(tbl) do
     if fn(key, val) then
@@ -8,6 +9,24 @@ function M.count(tbl, fn)
     end
   end
   return count
+end
+
+function M.sum(table, fn)
+  fn = fn or function(num) return num end
+  local sum = 0
+  for key, value in pairs(table) do
+    sum = sum + fn(value, key)
+  end
+  return sum
+end
+
+function M.product(table, fn)
+  fn = fn or function(num) return num end
+  local product = 1
+  for key, value in pairs(table) do
+    product = product * fn(value, key)
+  end
+  return product
 end
 
 return M
